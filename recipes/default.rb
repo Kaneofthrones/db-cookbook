c65d45f8cc9e0408a33228067f9ac0de72debd82
Kane Marcus <kaneofthrones@gmail.com>
2017-09-20 13:55:47 +0100

    version 0.3.0

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,8 +3,6 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-apt_update
-
 apt_repository 'mongodb-org' do 
   uri "http://repo.mongodb.org/apt/ubuntu"
   distribution "xenial/mongodb-org/3.2"
@@ -13,17 +11,25 @@ apt_repository 'mongodb-org' do
   key "EA312927"
 end
 
-package'mongodb' 
-
-template '/etc/mongod.conf' do
-    source 'mongod.conf.erb'
-end
+package 'mongodb-org'
 
 template '/lib/systemd/system/mongod.service' do
-    source 'mongod.service.erb'
+  source 'mongod.service.erb'
+  mode '0600'
+  owner 'root'
+  group 'root'
+  notifies :restart, 'service[mongod]'
+end
+
+template '/etc/mongod.conf' do
+  source 'mongod.conf.erb'
+  mode '0755'
+  owner 'root'
+  group 'root'
+  notifies :restart, 'service[mongod]'
 end
 
 service 'mongod' do 
-    supports status: true, restart: true
-    action [:enable, :start]    
+  supports status: true, restart: true, reload: true
+  action [:enable, :start]
 end
